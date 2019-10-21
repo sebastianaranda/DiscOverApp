ad69fd6777c2af744b80ad095977605237ea96c9
Sebastian aranda <sebastian.aranda.12@gmail.com>
2019-10-20 22:35:48 -0300

    Agrego navigationView con menu generico

--- app/src/main/java/com/digitalhouse/a0819cpmoacn02armo_01/view/MainActivity.java
+++ app/src/main/java/com/digitalhouse/a0819cpmoacn02armo_01/view/MainActivity.java
@@ -1,14 +1,22 @@
 package com.digitalhouse.a0819cpmoacn02armo_01.view;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.Fragment;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
 import com.digitalhouse.a0819cpmoacn02armo_01.R;
 import com.digitalhouse.a0819cpmoacn02armo_01.model.Artist;
 import com.digitalhouse.a0819cpmoacn02armo_01.model.Genre;
+import com.google.android.material.navigation.NavigationView;
 
-public class MainActivity extends AppCompatActivity implements ArtistsFragment.FragmentArtistsListener, GenresFragment.GenresFragmentListener {
+public class MainActivity extends AppCompatActivity implements ArtistsFragment.FragmentArtistsListener, GenresFragment.GenresFragmentListener, NavigationView.OnNavigationItemSelectedListener {
+
+    private DrawerLayout drawerLayout;
+    private NavigationView navigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,6 +24,9 @@ public class MainActivity extends AppCompatActivity implements ArtistsFragment.F
         setContentView(R.layout.activity_main);
         attachFragment(new ArtistsFragment());
         attachGenreFragment(new GenresFragment());
+        drawerLayout = findViewById(R.id.mainActivity_drawerLayout);
+        navigationView = findViewById(R.id.mainActivity_navigationView);
+        navigationView.setNavigationItemSelectedListener(this);
     }
 
     private void attachFragment(Fragment fragment) {
@@ -44,6 +55,28 @@ public class MainActivity extends AppCompatActivity implements ArtistsFragment.F
     @Override
     public void getGenreFromFragment(Genre genre) {
 
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        Integer id = menuItem.getItemId();
+        switch (id){
+            //TODO: Definir comportamiento del menu
+            case R.id.mainMenuFav:
+                Toast.makeText(this, "Seleccionaste el menu Favoritos", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.mainMenuProfile:
+                Toast.makeText(this, "Seleccionaste el menu Perfil", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.mainMenuSettings:
+                Toast.makeText(this, "Seleccionaste el menu Settings", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.mainMenuLogout:
+                Toast.makeText(this, "Seleccionaste el menu Logout", Toast.LENGTH_SHORT).show();
+                break;
+        }
+        drawerLayout.closeDrawers();
+        return false;
     }
 
 }
